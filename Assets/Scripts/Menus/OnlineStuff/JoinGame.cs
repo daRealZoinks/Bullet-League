2cf12f2cbb42f1f2a2b9413f5f01e9f3fbd62bc7
Zoinks <murarashuvlad@gmail.com>
2022-05-25 14:38:45 +0300

    zesxrctgbhjn

--- Assets/Scripts/Menus/OnlineStuff/JoinGame.cs
+++ Assets/Scripts/Menus/OnlineStuff/JoinGame.cs
@@ -1,15 +1,26 @@
+using Photon.Pun;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class JoinGame : MonoBehaviour
 {
     //list for buttons
 
+    private List<JoinGameButton> joinGameButtons;
+
     private void Start()
     {
-        //refresh the rooms list
+        RefreshRoomList();
     }
 
-    //refreshing room list function
-    //delete all current buttons
-    //make new buttons representing the rooms
+    private void RefreshRoomList()
+    {
+        joinGameButtons.Clear();
+
+        for (int i = 0; i < PhotonNetwork.CountOfRooms; i++)
+        {
+            joinGameButtons.Add();
+        }
+        //make new buttons representing the rooms
+    }
 }
