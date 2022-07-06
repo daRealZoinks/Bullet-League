ec390c77e9609c3de580f224b94ee136867ca811
Zoinks <murarashuvlad@gmail.com>
2022-07-06 17:42:24 +0300

    added an icon and some fonts

--- Assets/Scripts/Online/PlayerStart.cs
+++ Assets/Scripts/Online/PlayerStart.cs
@@ -5,6 +5,8 @@ public class PlayerStart : MonoBehaviourPunCallbacks
 {
     public Color color;
 
+    private Mesh mesh;
+
     private GameManager gameManager;
 
     public void SetGameManager(GameManager gameManager)
@@ -16,18 +18,28 @@ public class PlayerStart : MonoBehaviourPunCallbacks
     {
         if (PhotonNetwork.IsConnected)
         {
-            PhotonNetwork.Instantiate(gameManager.playerPrefab.name, transform.position, transform.rotation);
+            PhotonNetwork.Instantiate(gameManager.playerPrefab.name, transform.position - transform.up * transform.localScale.y / 2, transform.rotation);
         }
         else
         {
-            Instantiate(gameManager.playerPrefab, transform.position, transform.rotation);
+            Instantiate(gameManager.playerPrefab, transform.position - transform.up * transform.localScale.y / 2, transform.rotation);
         }
     }
 
     private void OnDrawGizmos()
     {
-        Gizmos.color = color;
-        Gizmos.DrawWireCube(transform.position + transform.up * transform.localScale.y / 2, transform.localScale);
-        Gizmos.DrawLine(transform.position + transform.up * transform.localScale.y / 2, transform.position + transform.up * transform.localScale.y / 2 + transform.forward * 1.5f);
+        Gizmos.color = Color.cyan;
+        // Gizmos.DrawLine(transform.position, transform.position + transform.forward * 1.5f);
+        Gizmos.DrawRay(transform.position, transform.forward * 1.5f);
+
+        Gizmos.color = new Color(color.r, color.g, color.b, 5f / 255);
+        Gizmos.DrawWireMesh(mesh, -1, transform.position, transform.rotation, transform.localScale);
+
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = new Color(color.r, color.g, color.b, 30f / 255);
+        Gizmos.DrawWireMesh(mesh, -1, transform.position, transform.rotation, transform.localScale);
     }
 }
