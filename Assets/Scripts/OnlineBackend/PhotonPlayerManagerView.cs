347041fc66e3f26f7f472e6513d8679eec9851d3
Zoinks <murarashuvlad@gmail.com>
2022-08-09 08:57:22 +0300

    player team color syncs between players

--- Assets/Scripts/OnlineBackend/PhotonPlayerManagerView.cs
+++ Assets/Scripts/OnlineBackend/PhotonPlayerManagerView.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using Photon.Pun;
 
-public class PhotonPlayerManagerView : MonoBehaviour, IPunObservable
+public class PhotonPlayerManagerView : MonoBehaviourPun, IPunObservable
 {
     private PlayerManager _playerManager;
 
@@ -9,16 +9,18 @@ public class PhotonPlayerManagerView : MonoBehaviour, IPunObservable
     {
         _playerManager = GetComponent<PlayerManager>();
     }
-    
+
     public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
-        if (stream.IsWriting)
+        if (photonView.AmOwner)
         {
             stream.SendNext(_playerManager.Team);
+            Debug.Log("Sending team: " + _playerManager.Team);
         }
         else
         {
             _playerManager.Team = (Team)stream.ReceiveNext();
+            Debug.Log("Receiving team: " + _playerManager.Team);
         }
     }
 }
